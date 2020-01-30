a35a054d0652a85425d7048f953e118b26951490
mauricio_ribeiro <mrexsolucoes@hotmail.com>
2020-01-30 16:03:58 -0300

    Foi incluída uma rotina para executar o sistema na barra de tarefas e uma rotina para executar a chamada de vários métodos.

--- MelFitnessAssinaturas/Form1.Designer.cs
+++ MelFitnessAssinaturas/Form1.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtChaveSecreta = new System.Windows.Forms.TextBox();
+            this.txtClienteId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(51, 51);
+            this.button1.Location = new System.Drawing.Point(136, 259);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -47,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(141, 51);
+            this.button2.Location = new System.Drawing.Point(226, 259);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(157, 23);
             this.button2.TabIndex = 1;
@@ -57,7 +64,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(304, 51);
+            this.button3.Location = new System.Drawing.Point(389, 259);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -67,7 +74,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(395, 51);
+            this.button4.Location = new System.Drawing.Point(480, 259);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(104, 23);
             this.button4.TabIndex = 3;
@@ -77,7 +84,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(505, 51);
+            this.button5.Location = new System.Drawing.Point(590, 259);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
@@ -85,19 +92,71 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chave Secreta";
+            // 
+            // txtChaveSecreta
+            // 
+            this.txtChaveSecreta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChaveSecreta.Location = new System.Drawing.Point(12, 27);
+            this.txtChaveSecreta.Name = "txtChaveSecreta";
+            this.txtChaveSecreta.Size = new System.Drawing.Size(204, 21);
+            this.txtChaveSecreta.TabIndex = 6;
+            // 
+            // txtClienteId
+            // 
+            this.txtClienteId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClienteId.Location = new System.Drawing.Point(222, 27);
+            this.txtClienteId.Name = "txtClienteId";
+            this.txtClienteId.Size = new System.Drawing.Size(204, 21);
+            this.txtClienteId.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Id Cliente";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(677, 294);
+            this.Controls.Add(this.txtClienteId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtChaveSecreta);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +167,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtChaveSecreta;
+        private System.Windows.Forms.TextBox txtClienteId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- MelFitnessAssinaturas/Form1.cs
+++ MelFitnessAssinaturas/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MelFitnessAssinaturas.Models;
 using MundiAPI.PCL;
 using MundiAPI.PCL.Models;
 
@@ -14,20 +15,121 @@ namespace MelFitnessAssinaturas
 {
     public partial class Form1 : Form
     {
+
+        private Timer _timer = new Timer();
+        private bool _allowVisible;
+        private bool _allowClose;
+
+
         public Form1()
         {
             InitializeComponent();
+            notifyIcon1.ContextMenu = new ContextMenu();
+            notifyIcon1.ContextMenu.MenuItems.Add("Abrir", abrir_Click);
+            notifyIcon1.ContextMenu.MenuItems.Add("Sair", sair_Click);
+            ExecutarTarefas();
         }
 
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!_allowVisible)
+            {
+                value = false;
+                if (!IsHandleCreated) CreateHandle();
+            }
+
+            base.SetVisibleCore(value);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (!_allowClose)
+            {
+                Hide();
+                e.Cancel = true;
+            }
+
+            base.OnFormClosing(e);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    SendKeys.Send("{TAB}");
+                    break;
+                case Keys.Escape:
+                    Close();
+                    break;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void abrir_Click(object sender, EventArgs e)
+        {
+            _allowVisible = true;
+            Show();
+            WindowState = FormWindowState.Normal;
+            Activate();
+        }
+
+        private void sair_Click(object sender, EventArgs e)
+        {
+            _allowClose = true;
+            Application.Exit();
+        }
+
+
+        private void ExecutarTarefas()
+        {
+
+            var interval = TimeSpan.FromSeconds(10);
+            var timer = new System.Timers.Timer(interval.TotalMilliseconds) { AutoReset = false };
+
+            timer.Elapsed += (sender, eventArgs) =>
+            {
+                var start = DateTime.Now;
+                try
+                {
+                    Task task1 = Task.Factory.StartNew(() => Tarefa1());
+                    Task task2 = Task.Factory.StartNew(() => Tarefa2());
+                    Task.WaitAll(task1, task2);
+                }
+                finally
+                {
+                    var elapsed = DateTime.Now - start;
+                    if (elapsed < interval)
+                        timer.Interval = (interval - elapsed).TotalMilliseconds;
+                    else
+                        timer.Interval = TimeSpan.FromSeconds(10).TotalMilliseconds;
+                    timer.Start();
+                }
+            };
+
+            timer.Start();
+
+        }
+        
+
+        private async Task Tarefa1()
+        {
+            notifyIcon1.ShowBalloonTip(20, @"Atenção !!", @"Tarefa 1.", ToolTipIcon.Info);
+        }
+
+        private async Task Tarefa2()
+        {
+            notifyIcon1.ShowBalloonTip(20, @"Atenção !!", @"Tarefa 2.", ToolTipIcon.Info);
+        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            string basicAuthUserName = "sk_test_J3xVZJPUBTWOnj6v";
-
-            // Senha em branco. Passando apenas a secret key
-            string basicAuthPassword = "";
-
-            var client = new MundiAPIClient(basicAuthUserName, basicAuthPassword);
-
+           
             var metadata = new Dictionary<string, string>();
             metadata.Add("id", "my_customer_id");
 
@@ -45,34 +147,38 @@ namespace MelFitnessAssinaturas
             {
                 HomePhone = new CreatePhoneRequest
                 {
-
                     AreaCode = "22",
                     CountryCode = "55",
                     Number = "27234577"
                 },
                 MobilePhone = new CreatePhoneRequest
                 {
-
                     AreaCode = "22",
                     CountryCode = "55",
                     Number = "999100201"
                 },
             };
 
-            var request = new CreateCustomerRequest
-            {
-                Name = "Carlos Andre de Souza Viana",
-                Email = "viana.mail@gmail.com",
-                Type = "individual",
-                Document = "07069543786",
-                Gender = "male",
-                Code = "MY_CUSTOMER_002",
-                Phones = phones,
-                Address = address,
-                Metadata = metadata
-            };
+            var cliente = new Cliente("Maurício Ribeiro", "mrexsolucoes@hotmail.com", "individual",
+                "00000000000","male","XXXX",metadata,address,phones);
+            
 
-            var response = client.Customers.CreateCustomer(request);
+            // Senha em branco. Passando apenas a secret key
+           
+            
+            //var request = new CreateCustomerRequest
+            //{
+            //    Name = "Carlos Andre de Souza Viana",
+            //    Email = "viana.mail@gmail.com",
+            //    Type = "individual",
+            //    Document = "07069543786",
+            //    Gender = "male",
+            //    Code = "MY_CUSTOMER_002",
+            //    Phones = phones,
+            //    Address = address,
+            //    Metadata = metadata
+            //};
+            
 
             //return View();
 
@@ -150,8 +256,10 @@ namespace MelFitnessAssinaturas
 
             foreach (GetAccessTokenResponse item in response.Data)
             {
-                Console.WriteLine(string.Format("Id: {0} -- {1}", item.Id, item.Code));
+                Console.WriteLine($@"Id: {item.Id} -- {item.Code}");
             }
         }
+
+       
     }
 }
