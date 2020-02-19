c9b2109f2b84c7599cdfdb44ffac2475fe8ab9ed
carlosviana <viana.mail@gmail.com>
2020-02-18 21:32:38 -0300

    merge

---
+++ MelFitnessAssinaturas/Enums/TipoEnum.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace MelFitnessAssinaturas.Enums
+{
+    public enum TipoEnum
+    {
+        [Description("TODOS")]
+        To,
+        [Description("CLIENTE")]
+        Cl,
+        [Description("MEIO DE PAGAMENTO")]
+        Mp,
+        [Description("FATURAMENTO")]
+        Ft,
+        [Description("PLANO DE PAGAMENTO")]
+        Pl,
+        [Description("OUTRAS OPERAÇÕES")]
+        Oo,
+        [Description("ASSINATURA")]
+        As,
+        [Description("ERRO SISTÊMICO")]
+        Er
+    }
+}
