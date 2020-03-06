aee738f8bb3c132c21fa9b6f9afe11043ab16069
mauricio-ribeiro <mrexsolucoes@hotmail.com>
2020-03-06 15:54:51 -0300

    Commit de atualização - Código comentado
    Comentei umas linhas de código na classe MainController

--- MelFitnessAssinaturas/Controllers/MainController.cs
+++ MelFitnessAssinaturas/Controllers/MainController.cs
@@ -54,7 +54,7 @@ namespace MelFitnessAssinaturas.Controllers
                             break;
                         // Foi alterada a data de pagamento da assinatura  
                         case "A_EDP ":
-                            Console.WriteLine(@"Alterando a data de faturamento da assinatura");
+                            Console.WriteLine(@"Alterando a data de faturamento da assinatura");  // Não foi testado por completo.
                             AssinaturaCtrl.AlterarDataFaturameto(evento.IdTabela);
                             EventoDAL.MarcaRegistroProcessadoComo("P", evento.Id_Guid);
                             break;
@@ -64,13 +64,13 @@ namespace MelFitnessAssinaturas.Controllers
                             AssinaturaCtrl.CancelarAssinaturaApi(evento.IdTabela);
                             EventoDAL.MarcaRegistroProcessadoComo("P", evento.Id_Guid);
                             break;
-                        case "A_EDTI":
-                            //1 - Não é possível atualizar a data de início da assinatura para o dia anterior ao dia atual.
-                            //2 - Não é possível atualizar a data de início de uma assinatura que já começou.
-                            Console.WriteLine(@"Alterando a data de início da assinatura");
-                            AssinaturaCtrl.AlterarDataInicio(evento.IdTabela, Convert.ToInt32(evento.CodAux1));
-                            EventoDAL.MarcaRegistroProcessadoComo("P", evento.Id_Guid);
-                            break;
+                        //case "A_EDTI":
+                        //    //1 - Não é possível atualizar a data de início da assinatura para o dia anterior ao dia atual.
+                        //    //2 - Não é possível atualizar a data de início de uma assinatura que já começou.
+                        //    Console.WriteLine(@"Alterando a data de início da assinatura");
+                        //    AssinaturaCtrl.AlterarDataInicio(evento.IdTabela, Convert.ToInt32(evento.CodAux1));
+                        //    EventoDAL.MarcaRegistroProcessadoComo("P", evento.Id_Guid);
+                        //    break;
                         //  Foi soliticado renovação do ciclo de assinatura. Reportar a API.
                         case "A_RC":
 
